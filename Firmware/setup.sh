4e10a7375baea6bdc8e494e8066b317bc70c5aef
ashleyf <ashley.feniello@live.com>
2017-01-28 23:13:19 -0800

    Removed Reflecta

--- Firmware/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# install Brief library
-cp -r libraries/Brief/ ~/sketchbook/libraries/
-
-# install Jay Beaver's Reflecta library
-git clone https://github.com/JayBeavers/Reflecta/ ~/sketchbook/libraries/Reflecta/
